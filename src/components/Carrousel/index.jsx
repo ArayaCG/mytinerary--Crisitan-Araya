52c461c0ee0f4ac225ba8893e3716c6ba313bd01
Knutt64 <arayag64@gmail.com>
2023-08-10 13:24:38 -0300

    Corrección
    gitignore y más data

--- src/components/Carrousel/index.jsx
+++ src/components/Carrousel/index.jsx
@@ -9,19 +9,27 @@ import Venice from '../../assets/imgPlaces/venecia.jpg'
 import Tuskany from '../../assets/imgPlaces/Toscana.jpg'
 import Oaxaca from '../../assets/imgPlaces/oaxaca.jpg'
 import Sonora from '../../assets/imgPlaces/Sonora.jpg'
+import Bogota from '../../assets/imgPlaces/Bogota.jpeg'
+import machupichu from '../../assets/imgPlaces/machupichu.jpg'
+import oregon from '../../assets/imgPlaces/oregon.jpg'
+import viñamar from '../../assets/imgPlaces/viñamar.jpg'
 import Carro from './Carro'
 
 function Carrousel() {
   
   let data = [
-    {name: 'Buenos Aires',place: 'Argentina',img: Bsas, key: '1' },
-    {name: 'Mendoza',place: 'Argentina',img: Mdz , key: '2'},
-    {name: 'Tokio',place: 'Japan',img: Tokio, key: '3' },
-    {name: 'Iwate',place: 'Japan',img: Iwate, key: '4' },
-    {name: 'Venice',place: 'Italy',img: Venice, key: '5' },
-    {name: 'Tuskany',place: 'Italy',img: Tuskany, key: '6' },
-    {name: 'Oaxaca',place: 'Mexico',img: Oaxaca, key: '7' },
-    {name: 'Sonora',place: 'Mexico',img: Sonora, key: '8' },
+    {name: 'Buenos Aires',place: 'Argentina',img: Bsas},
+    {name: 'Mendoza',place: 'Argentina',img: Mdz},
+    {name: 'Tokio',place: 'Japan',img: Tokio},
+    {name: 'Iwate',place: 'Japan',img: Iwate},
+    {name: 'Venice',place: 'Italy',img: Venice},
+    {name: 'Tuskany',place: 'Italy',img: Tuskany},
+    {name: 'Oaxaca',place: 'Mexico',img: Oaxaca},
+    {name: 'Sonora',place: 'Mexico',img: Sonora},
+    {name: 'Bogota',place: 'Colobia',img: Bogota},
+    {name: 'Machu Pichu',place: 'Perú',img: machupichu},
+    {name: 'Oregon',place: 'U.S.A.',img: oregon},
+    {name: 'Viña del Mar',place: 'Chile',img: viñamar},
 ]
 
 
